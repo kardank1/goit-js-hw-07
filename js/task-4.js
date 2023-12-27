a0be27d51535b0d1a0540826c7250ff63a23ce5f
kardank1 <kaplun.danil.2001@gmail.com>
2023-12-27 17:04:40 +0200

    4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", sendForm);
+
+function sendForm(event){
+    event.preventDefault();
+    const form = event.target;
+    if(form.elements.email.value == "" || form.elements.password.value == ""){
+        return alert('All form fields must be filled in');
+    }
+
+    const formInfo = {};
+    formInfo.email = form.elements.email.value.trim();
+    formInfo.password = form.elements.password.value.trim();
+    console.log(formInfo);
+    loginForm.reset();
+}
